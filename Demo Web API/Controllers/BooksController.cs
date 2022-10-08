aa3dcaa89d71d34f6106c6b61ead5df461056f4c
Mrunal77 <mtmeshram77@outlook.com>
2022-10-08 22:01:20 +0530

    Add project files.

---
+++ Demo Web API/Controllers/BooksController.cs
@@ -0,0 +1,69 @@
+﻿using Demo_Web_API.DTO;
+using Demo_Web_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo_Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet(Name = "GetBook")]
+        public ActionResult GetBookData()
+        {
+            var books = new List<Book>();
+            using (var context = new LibraryContext())
+            {
+                books = context.Book
+                   .Include(p => p.Publisher).ToList();
+
+            }
+
+            return Ok(books.ToArray());
+        }
+
+        [HttpPost(Name = "AddBook")]
+        public ActionResult AddBook(BookRequestBody book)
+        {
+            var bookresponse = new Book();
+
+            using (var context = new LibraryContext())
+            {
+                // Creates the database if not exists
+                context.Database.EnsureCreated();
+
+                // Adds a publisher
+                var publisher = new Publisher
+                {
+                    Name = book.Publisher
+                };
+                context.Publisher.Add(publisher);
+
+                // Adds some books
+                context.Book.Add(new Book
+                {
+                    ISBN = book.ISBN,
+                    Title = book.Title,
+                    Author = book.Author,
+                    Language = book.Language,
+                    Pages = book.Pages,
+                    Publisher = publisher
+                });               
+
+                // Saves changes
+                context.SaveChanges();
+
+                bookresponse.ISBN = book.ISBN;
+                bookresponse.Title = book.Title;
+                bookresponse.Author = book.Author;
+                bookresponse.Language = book.Language;
+                bookresponse.Pages = book.Pages;
+                bookresponse.Publisher = publisher;
+            }
+
+            return Ok(book);
+        }
+    }
+}
---
+++ Demo Web API/Models/LibraryContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySql.EntityFrameworkCore.Extensions;
+using System.Security.Policy;
+
+namespace Demo_Web_API.Models
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Book> Book { get; set; }
+
+        public DbSet<Publisher> Publisher { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;port=3306;database=library;user=root;password=root");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Publisher>(entity =>
+            {
+                entity.HasKey(e => e.ID);
+                entity.Property(e => e.Name).IsRequired();
+            });
+
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.HasKey(e => e.ISBN);
+                entity.Property(e => e.Title).IsRequired();
+                entity.HasOne(d => d.Publisher)
+                  .WithMany(p => p.Books);
+            });
+        }
+    }
+}
